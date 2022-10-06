61caf4cbbda89bb4c8d47af403f100040aff7e0d
nemet92 <pasayev.nemet@mail.ru>
2022-10-07 02:14:50 +0400

    lessonLoginPage

--- lib/loginPageView.dart
+++ lib/loginPageView.dart
@@ -24,7 +24,7 @@ class _LoginPageViewState extends State<LoginPageView> {
             children: [
               const Text(
                 textProje.login,
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 21),
               ),
               customSizedBox(context),
               RichText(
